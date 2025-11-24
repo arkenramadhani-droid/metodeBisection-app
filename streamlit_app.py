a036f9d90b9232295ca11fd7850792f9c04752d3
arkenramadhani-droid <arken.ramadhani@gmail.com>
2025-11-24 12:58:04 +0700

    Update app title and introductory text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("SPNL with meotde Bisection")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "by Arya Kendrian, follow my Instagram [https://www.instagram.com/aryakndrn/](https://www.instagram.com/aryakndrn/)."
 )
