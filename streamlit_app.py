cdda1855b164a378727bd3549907a4492d18ba14
arkenramadhani-droid <arken.ramadhani@gmail.com>
2025-11-25 03:50:56 +0700

    Remove markdown for 'TUGAS UAS ARYA KENDRIAN'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title("SPNL with meotde Bisection")
-st.markdown("TUGAS UAS ARYA KENDRIAN")
 st.markdown(
     '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">',
     unsafe_allow_html=True
