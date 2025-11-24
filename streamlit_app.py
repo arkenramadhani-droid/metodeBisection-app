2cf91801bab32db2cd856312360356c28fb40afb
arkenramadhani-droid <arken.ramadhani@gmail.com>
2025-11-24 13:06:15 +0700

    Update title and Instagram link formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
 st.title("SPNL with meotde Bisection")
-st.write(
-    "by Arya Kendrian, follow my Instagram [https://www.instagram.com/aryakndrn/](https://www.instagram.com/aryakndrn/)."
-)
+st.markdown("TUGAS UAS ARYA KENDRIAN")
+st.markdown("follow Instagram saya: [aryakndrn](https://www.instagram.com/aryakndrn/)")
